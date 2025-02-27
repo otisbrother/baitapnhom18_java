5a8bccbbd92e3452fe67df33f23ff968799d5973
Nguyễn Huy Tỏa <104702066+otisbrother@users.noreply.github.com>
2025-02-27 21:05:48 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main {
         classrooms.put("HTTT1", new Classroom("HTTT1"));
         classrooms.put("HTTT3", new Classroom("HTTT3"));
 
-        classrooms.get("HTTT1").addStudent(new Student("Nguyen Thien", "Huong", "2002-05-10", "Ha Noi", "HTTT1", 9, 8.5, 8, 9, 8.5));
-        classrooms.get("HTTT1").addStudent(new Student("Nguyen Huy", "Toa", "2002-06-15", "HCM", "HTTT1", 7, 6.5, 7, 7.5, 6));
-        classrooms.get("HTTT3").addStudent(new Student("Nguyen Khanh", "Huyen", "2002-07-20", "Da Nang", "HTTT3", 5, 5.5, 6, 4, 3.5));
+        classrooms.get("HTTT1").addStudent(new Student("Nguyen Huy", "Toa", "2002-05-10", "Ha Noi", "HTTT1", 9, 8.5, 8, 9, 8.5));
+        classrooms.get("HTTT1").addStudent(new Student("Nguyen Manh", "Thang", "2002-06-15", "HCM", "HTTT1", 7, 6.5, 7, 7.5, 6));
+        classrooms.get("HTTT3").addStudent(new Student("Dao Dinh ", "Hung", "2002-07-20", "Da Nang", "HTTT3", 5, 5.5, 6, 4, 3.5));
        
         while (true) {
             System.out.print("\nNhap ma lop can xem (HTTT1, HTTT3) hoac 'exit' de thoat: ");
